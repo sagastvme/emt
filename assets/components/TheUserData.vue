d652e5c1346e9568f6fd42170e50ca9d25871120
Tu Nombre <miltormentas@tutanota.com>
2023-03-27 14:15:21 -0700

    I have added the plans and prices buttons

--- assets/components/TheUserData.vue
+++ assets/components/TheUserData.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <button @click="test()">Load data</button>
+
     <div v-if="myData">
       <table>
         <tr>
-          <td>Username: {{ username }}</td>
+          <td>Username: </td>
+          <td>{{ username }}</td>
         </tr>
         <tr>
-          <td>Password: {{ password }}</td>
+          <td>
+            Foto de perfil:
+          </td>
+          <td>
+            <img :src="profilePic" alt="">
+          </td>
         </tr>
         <tr>
-          Foto de perfil: <img :src="profilePic" alt="">
+          <td>
+            <button @click="changePassword" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
+              Click me
+            </button>
+          </td>
         </tr>
       </table>
     </div>
-  </div>
 </template>
 
 <script>
@@ -24,7 +32,6 @@ export default {
     return {
       myData: false,
       username: '',
-      password: '',
       profilePic: '',
     }
   },
@@ -32,12 +39,12 @@ export default {
     console.log('guardado')
     // Check if data is in localStorage
     const username = localStorage.getItem('username');
-    const password = localStorage.getItem('password');
+
     const profilePic = localStorage.getItem('profilePicture');
-    if (username && password && profilePic) {
+    if (username &&  profilePic) {
       // If data is in localStorage, use it
       this.username = username;
-      this.password = password;
+
       this.profilePic = profilePic;
       this.myData = true;
     } else {
@@ -48,25 +55,21 @@ export default {
   },
   methods: {
     fetchData() {
-      axios.get('/test')
+      axios.get('/userData')
           .then(response => {
             // Save data to localStorage
             localStorage.setItem('username', response.data.username);
-            localStorage.setItem('password', response.data.password);
+
             localStorage.setItem('profilePicture', response.data.profilePicture);
             // Set data properties
             this.username = response.data.username;
-            this.password = response.data.password;
+
             this.profilePic = response.data.profilePicture;
             this.myData = true;
           })
           .catch(error => {
             console.error(error);
           });
-    },
-    test() {
-      // Call fetchData() to fetch data from server and save it to localStorage
-      this.fetchData();
     }
   }
 }
